48e53f507634e2a2064fe65c15ebf0c314281183
Umihayati <rehimayesuf09@gmail.com>
2025-09-19 18:30:39 +0300

    week 7 task

---
+++ task.js
@@ -0,0 +1,12 @@
+// Task object
+let task1={
+    id:1,
+    name:"Do homework",
+    completed:true
+};
+// to show different messages
+if (task1.completed === true){
+    console.log("Task is completed");
+} else{
+    console.log("Task is still pending");
+}
