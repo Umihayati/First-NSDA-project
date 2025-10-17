6b167d548b3581a46319d78f94bd924eab1ef36a
Umihayati <rehimayesuf09@gmail.com>
2025-10-17 16:50:08 +0300

    task 8

---
+++ taskloop.js
@@ -0,0 +1,5 @@
+let tasks = ["Do homework", "Clean the room", "Buy groceries", "Study JavaScript"];
+console.log("My Tasks:");
+for (let i = 0; i < tasks.length; i++) {
+    console.log(`${i + 1}. ${tasks[i]}`);
+}
